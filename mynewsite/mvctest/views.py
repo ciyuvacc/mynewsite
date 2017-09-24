b91fd11b4efc3854d66a8d49b03dcedaf84a67cb
ciyuvacc <850533921@qq.com>
2017-09-24 12:03:05 +0000

    201709303

--- mynewsite/mvctest/views.py
+++ mynewsite/mvctest/views.py
@@ -1,19 +1,30 @@
+#coding:utf8
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from django.template.loader import get_template
+import random
+#
+#def about(request):
+#    html = '''
+#<!DOCTYPE html>
+#<html>
+#<head><title> About Myself</title></head>
+#<body>
+#<h2>Min-Huang Ho</h2>
+#<hr>
+#<p>
+#Hi, I am Min-Huang Ho,Nice to meet you!
+#</p>
+#</body>
+#</html>
+#'''
+#    return HttpResponse(html)
 
-from django.http import HttpResponse
-
-def about(request):
-    html = '''
-<!DOCTYPE html>
-<html>
-<head><title> About Myself</title></head>
-<body>
-<h2>Min-Huang Ho</h2>
-<hr>
-<p>
-Hi, I am Min-Huang Ho,Nice to meet you!
-</p>
-</body>
-</html>
-'''
+def about(requtest):
+    template = get_template('about.html')
+    quotes = [ '永远不要向任何人解释你自己。因为喜欢你的人不需要，而不喜欢你的人不会相信.',
+    '别让某人成为你生命中的优先，当你只是他们生命中的一个选择时。人与人之间的关系只有在彼此达到平衡时，运作的最恰当.',
+    '生活不是单行线，一条路走不通，你可以转弯.',
+    '让梦想成真的最好办法就是醒来.']
+    html = template.render({'quote':random.choice(quotes)})
     return HttpResponse(html)
--- mynewsite/phone/views.py
+++ mynewsite/phone/views.py
@@ -1,57 +1,97 @@
 #coding:utf8
 from django.shortcuts import render
 from django.http import HttpResponse,Http404
+from django.template.loader import get_template
 from .models import Phonelist
 
 # Create your views here.
+#def listting(request):
+#    html = '''
+#<!DOCTYPE html>
+#<html>
+#<head>
+#<meta  charset='utf-8'>
+#<title>中古手机列表</title>
+#</head>
+#<body>
+#<h2>以下是目前本店销售中的二手手机列表</h2>
+#<table width=400 border=1 bgcolor='#ccffcc'>
+#{}
+#</table>
+#</body>
+#'''
+#    products = Phonelist.objects.all()
+#    tags = '<tr><td>产品</td><td>售价</td><td>库存量</td></tr>'
+#    for p in products:
+#        tags = tags + '<tr><td>{}</td>'.format(p.name)
+#        tags = tags + '<td>{}</td>'.format(p.price)
+#        tags = tags + '<td>{}</tr>'.format(p.qty)
+#    return HttpResponse(html.format(tags))
+
+
+
+
 def listting(request):
-    html = '''
-<!DOCTYPE html>
-<html>
-<head>
-<meta  charset='utf-8'>
-<title>中古手机列表</title>
-</head>
-<body>
-<h2>以下是目前本店销售中的二手手机列表</h2>
-<table width=400 border=1 bgcolor='#ccffcc'>
-{}
-</table>
-</body>
-'''
     products = Phonelist.objects.all()
-    tags = '<tr><td>产品</td><td>售价</td><td>库存量</td></tr>'
-    for p in products:
-        tags = tags + '<tr><td>{}</td>'.format(p.name)
-        tags = tags + '<td>{}</td>'.format(p.price)
-        tags = tags + '<td>{}</tr>'.format(p.qty)
-    return HttpResponse(html.format(tags))
+    template = get_template('list.html')
+    html =  template.render({'products':products})
+    return HttpResponse(html)
+
+#def disp_detail(request,sku):
+#    html = '''
+#<!DOCTYPE html>
+#<html>
+#<head>
+#<meta  charset='utf-8'>
+#<title>{}</title>
+#</head>
+#<body>
+#<h2>{}</h2>
+#<hr>
+#<table width=400 border=1 bgcolor='#ccffcc'>
+#{}
+#</table>
+#<a href='/list'>返回列表</a>
+#</body>
+#</html>
+#'''
+#    try:
+#        p = Phonelist.objects.get(sku=sku)
+#    except Phonelist.DoesNotExist:
+#        raise Http404('找不到指定的产品编号')
+#    tags = '<tr><td>产品编号</td><td>{}</td></tr>'.format(p.sku)
+#    tags = tags + '<tr><td>产品名称</td><td>{}</td></tr>'.format(p.name)
+#    tags = tags + '<tr><td>二手价格</td><td>{}</td></tr>'.format(p.price)
+#    tags = tags + '<tr><td>库存数量</td><td>{}</td></tr>'.format(p.qty)
+#    #return HttpResponse(html.format(p.sku,p.name,tags))
+#    return HttpResponse(html.format(p.sku,p.name,tags))
+
+
+
+
+
+#def disp_detail(request,sku):
+#    try:
+#        p = Phonelist.objects.get(sku=sku)
+#    except Phonelist.DoesNotExist:
+#        raise Http404('找不到指定的产品编号')
+#    tags = '<tr><td>产品编号</td><td>{}</td></tr>'.format(p.sku)
+#    tags = tags + '<tr><td>产品名称</td><td>{}</td></tr>'.format(p.name)
+#    tags = tags + '<tr><td>二手价格</td><td>{}</td></tr>'.format(p.price)
+#    tags = tags + '<tr><td>库存数量</td><td>{}</td></tr>'.format(p.qty)
+#    template = get_template('disp.html')
+#    html = template.render({'pname':p.name,'tags':tags})
+#    return HttpResponse(html)
+
+
+
 
 def disp_detail(request,sku):
-    html = '''
-<!DOCTYPE html>
-<html>
-<head>
-<meta  charset='utf-8'>
-<title>{}</title>
-</head>
-<body>
-<h2>{}</h2>
-<hr>
-<table width=400 border=1 bgcolor='#ccffcc'>
-{}
-</table>
-<a href='/list'>返回列表</a>
-</body>
-</html>
-'''
     try:
         p = Phonelist.objects.get(sku=sku)
     except Phonelist.DoesNotExist:
         raise Http404('找不到指定的产品编号')
-    tags = '<tr><td>产品编号</td><td>{}</td></tr>'.format(p.sku)
-    tags = tags + '<tr><td>产品名称</td><td>{}</td></tr>'.format(p.name)
-    tags = tags + '<tr><td>二手价格</td><td>{}</td></tr>'.format(p.price)
-    tags = tags + '<tr><td>库存数量</td><td>{}</td></tr>'.format(p.qty)
-    #return HttpResponse(html.format(p.sku,p.name,tags))
-    return HttpResponse(html.format(p.sku,p.name,tags))
+    template = get_template('disp.html')
+    html = template.render({'product':p})
+  
+    return HttpResponse(html)
